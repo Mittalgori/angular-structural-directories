a623e15540a42fdab01cecda9cc47977bed08870
Mittalgori <mitu796@gmail.com>
2024-04-29 10:03:09 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'structural-directives-demo';
+  num: number = 0;
+  isEvenOdd(): boolean{
+    return this.num % 2 == 0;
+  }
+
+  fruits: string[] = ["Apple", "Kiwi", "Oranges", "Banana"];
+  newFruitValue: string = "";
+  addFruit(){
+
+    this.fruits.push(this.newFruitValue)
+
+  }
+
+  selectedColor: string = "red";
+  setColor(event: Event){
+    const target = event.target as HTMLSelectElement;
+    this.selectedColor = target.value;
+  }
+
+
 }
